ab1c61ea437ebdc8d52e3ea8654552e72986a57a
Michel Fournier <m.fournier.a@gmail.com>
2024-04-22 09:16:12 -0400

    user profile updates

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -13,6 +13,25 @@ export const validateMyUserRequest = [
     body("name").isString().notEmpty().withMessage("Name must be a string"),
     body("addressLine1").isString().notEmpty().withMessage("AddressLine1 must be a string"),
     body("city").isString().notEmpty().withMessage("City must be a string"),
-    body("country").isString().notEmpty().withMessage("COuntry must be a string"),
+    body("country").isString().notEmpty().withMessage("Country must be a string"),
     handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").isString().notEmpty().withMessage("Restaurant name is required"),
+    body("city").isString().notEmpty().withMessage("City is required"),
+    body("country").isString().notEmpty().withMessage("Country is required"),
+    body("deliveryPrice").isFloat({ min: 0}).withMessage("Delivery price must be a positive number"),
+    body("estimatedDeliveryTime").isInt({ min: 0}).withMessage("Estimated Delivery time must be a positive integer"),
+    body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not().isEmpty()
+    .withMessage("Cuisines array must not be empty"),
+    body("menuItems")
+    .isArray()
+    .withMessage("Menu items must be an array"),
+    body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+    body("menuItems.*.price").isFloat({ min: 0}).withMessage("Menu price is required and must be a positive number"),
+    handleValidationErrors,
+]
